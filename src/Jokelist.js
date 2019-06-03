6e5d2a41d778ee55b4527d6d3da803764346d6ae
Joshua Kaluski <joshua@kaluski.com>
2019-06-03 19:32:51 -0400

    Added voting system and improved styling.

--- src/Jokelist.js
+++ src/Jokelist.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import Joke from './Joke';
 import axios from 'axios';
+import uuid from 'uuid/v4';
 import './Jokelist.css';
 
 class Jokelist extends Component {
@@ -20,15 +22,21 @@ class Jokelist extends Component {
         //Generates 10 jokes
         while (jokes.length < this.props.numJokesToGet) {
             let res = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept: "application/json"}});
-            jokes.push(res.data.joke);
+            jokes.push({id: uuid(), text: res.data.joke, votes: 0});
         }
-
+        console.log(jokes);
         //Set state jokes to local variable jokes (add the jokes to the state)
         this.setState({jokes: jokes});
     }
-
+    handleVote(id, delta) {
+        this.setState(
+            st => ({
+                jokes: st.jokes.map(joke =>
+                joke.id === id ? {...joke, votes: joke.votes + delta} : joke)
+            })
+        )
+    }
     render() {
-
         return (
             <div className="Jokelist">
                 <div className="Jokelist-sidebar">
@@ -36,10 +44,15 @@ class Jokelist extends Component {
                     <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="Laughing emoji"/>
                     <button className="Jokelist-new">New Jokes</button>
                 </div>
-
                 <div className="Jokelist-jokes">
                     {this.state.jokes.map(joke => (
-                        <div>{joke}</div>
+                        <Joke
+                            key={joke.id}
+                            text={joke.text}
+                            votes={joke.votes}
+                            upvote={() => this.handleVote(joke.id, 1)}
+                            downvote={() => this.handleVote(joke.id, -1)}
+                        />
                     ))}
                 </div>
             </div>
